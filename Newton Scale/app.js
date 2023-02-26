c5101664e453b98506fd29ebc56b6e298114dca9
Ersin Göç <ersin_1229@hotmail.com>
2023-02-26 22:13:11 +0300

    newton scale

---
+++ Newton Scale/app.js
@@ -0,0 +1,9 @@
+//Celvin
+const kelvin = 0;
+//Celcius
+const celcius = kelvin - 273;
+//Newton
+let newton = celcius * (33/100);
+newton = Math.floor(newton);
+//Output newton scale
+console.log(`The newton scale ${newton}`);
