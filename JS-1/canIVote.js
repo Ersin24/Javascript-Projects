02febb1af3861d77aab2fe4ba3955dd68850fcf4
Ersin Göç <ersin_1229@hotmail.com>
2023-04-09 21:09:31 +0300

    aggreeOrDisagree and canIVote added

---
+++ JS-1/canIVote.js
@@ -0,0 +1,20 @@
+// Write your function here:
+const canIVote = age => {
+    if(age >= 18){
+      return true;
+    }else if(age < 18){
+      return false;
+    }
+  }
+  
+  console.log(canIVote(17));
+  
+  
+  
+  
+  
+  // Uncomment the line below when you're ready to try out your function
+  // console.log(canIVote(19)) // Should print true
+  
+  // We encourage you to add more function calls of your own to test your code!
+  
