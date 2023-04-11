efe5cad7b807f086ee193ad4350c727f4c54565b
Ersin Göç <ersin_1229@hotmail.com>
2023-04-11 16:30:29 +0300

    silly Sentence project app

---
+++ JS-1/sillySentence.js
@@ -0,0 +1,16 @@
+// Write your function here:
+
+const sillySentence = (val1, val2, val3) => {
+    return `I am so ${val1} because I ${val2} coding! Time to write some more awesome ${val3}!`;
+  }
+  
+  
+  console.log(sillySentence('excited', 'love', 'functions'))
+  
+  
+  
+  // Uncomment the line below when you're ready to try out your function
+  // console.log(sillySentence('excited', 'love', 'functions')) 
+  
+  // We encourage you to add more function calls of your own to test your code!
+  
