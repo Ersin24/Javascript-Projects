f77457bf92b0b4c8651ee4c753b1439a666f7ca0
Ersin Göç <ersin_1229@hotmail.com>
2023-05-11 23:40:43 +0300

    is Dinner Vegan Func added

---
+++ JS-2/AllChallanges.js/isDinnerVegan.js
@@ -0,0 +1,17 @@
+// Write your code here:
+const dinner = [{name: 'hamburger', source: 'meat'}, {name: 'cheese', source: 'dairy'}, {name: 'ketchup', source:'plant'}, {name: 'bun', source: 'plant'}, {name: 'dessert twinkies', source:'unknown'}];
+
+const isTheDinnerVegan = arr => arr.every(meal => meal.source === 'plant' ? true : false)
+
+
+console.log(isTheDinnerVegan(dinner))
+
+
+
+// Feel free to comment out the code below to test your function
+/*
+const dinner = [{name: 'hamburger', source: 'meat'}, {name: 'cheese', source: 'dairy'}, {name: 'ketchup', source:'plant'}, {name: 'bun', source: 'plant'}, {name: 'dessert twinkies', source:'unknown'}];
+
+console.log(isTheDinnerVegan(dinner))
+// Should print false
+*/
