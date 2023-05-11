8a70e9c457ec2a92e7b3cc0812181325dc4b9ca3
Ersin Göç <ersin_1229@hotmail.com>
2023-05-11 23:51:16 +0300

    find my keys added

---
+++ JS-2/AllChallanges.js/findMyKeys.js
@@ -0,0 +1,17 @@
+// Write your code here:
+const randomStuff = ['credit card', 'screwdriver', 'receipt', 'gum', 'keys', 'used gum', 'plastic spoon'];
+
+const findMyKeys = text =>  text.indexOf('keys')
+
+console.log(findMyKeys(randomStuff))
+
+
+
+
+// Feel free to comment out the code below to test your function
+/*
+const randomStuff = ['credit card', 'screwdriver', 'receipt', 'gum', 'keys', 'used gum', 'plastic spoon'];
+
+console.log(findMyKeys(randomStuff))
+// Should print 4
+*/
