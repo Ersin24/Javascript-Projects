7b1465608939848dc4e33206614b5e0d7e68f9ef
Ersin Göç <ersin_1229@hotmail.com>
2023-06-16 18:18:18 +0300

    some dom manipulating added

---
+++ InteractiveWebsites/DOM/2/script.js
@@ -0,0 +1,7 @@
+let newAttraction = document.createElement('li');
+
+newAttraction.id = 'vespa';
+newAttraction.innerHTML = 'Rent a Vespa';
+
+document.getElementById('italy-attractions').appendChild(newAttraction)
+
