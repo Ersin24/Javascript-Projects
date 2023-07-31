efc25a82c99197e2a4014ba9aa327515e9f9737b
Ersin Göç <ersin_1229@hotmail.com>
2023-07-31 17:59:53 +0300

    jsx added and react introduction

---
+++ ReactPart1/JSX/Ex2/app.js
@@ -0,0 +1,56 @@
+//CONDITIONAL 1
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+function coinToss() {
+  // This function will randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+};
+let img;
+
+// if/else statement begins here:
+if(coinToss() === 'heads'){
+  img = (
+    <img
+      src={pics.kitty}
+    />
+  );
+}else{
+  img = (
+    <img 
+      src={pics.doggy}
+    />
+  );
+}
+
+root.render(img)
+
+//CONDITIONAL 2
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+function coinToss () {
+  // Randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+};
+
+const img = <img src={pics[coinToss() === 'heads' ? 'kitty' : 'doggy']} />;
+
+root.render(img);
+
+//CONDITIONAL 3
